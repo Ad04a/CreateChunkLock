6e7db0d416d592ce6c9f41bf465838a796f98ad1
Ad04a <andrey.todorov04@gmail.com>
2025-09-30 23:47:08 +0300

    difficulty scaling

--- kubejs/server_scripts/bosses.js
+++ kubejs/server_scripts/bosses.js
@@ -1,4 +1,12 @@
 
+ServerEvents.loaded(event => {
+  let data = event.server.persistentData;
+  if(!data.DifficultyScaling)
+  {
+    data.DifficultyScaling = 0;
+  }
+
+})
 
 ServerEvents.entityLootTables(event => {
   for(let [bossId, dropAmounts] of Object.entries(global.bossIds)) {
@@ -11,3 +19,24 @@ ServerEvents.entityLootTables(event => {
     })
   };
 })
+
+EntityEvents.death(event => {
+  if (event.entity.type in global.bossIds && event.level.dimension != "bl:blueprint_dimension") {
+    let data = event.server.persistentData;
+    data.DifficultyScaling = (data.DifficultyScaling || 0) + global.bossIds[event.entity.type][1];
+    event.server.tell("creep " + event.level.dimension + "new difficulty: " + data.DifficultyScaling);
+  }
+});
+
+// Scale zombie HP on spawn
+EntityEvents.spawned(event => {
+  /*if (event.entity.type == "minecraft:zombie") {
+    let data = event.server.persistentData;
+    let kills = data.zombieKills || 0;
+
+    // base health = 20, +0.5 per kill
+    let newHealth = 20 + (kills * 0.5);
+    event.entity.maxHealth = newHealth;
+    event.entity.health = newHealth;
+  }*/
+});
