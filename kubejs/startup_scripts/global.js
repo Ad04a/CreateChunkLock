63e8a6276dfb0dab9750330551c1026f9accc1ff
Ad04a <andrey.todorov04@gmail.com>
2025-10-04 03:09:24 +0300

    registered all the mod bosses

--- kubejs/startup_scripts/global.js
+++ kubejs/startup_scripts/global.js
@@ -1,6 +1,34 @@
 global.bossIds = {
-  "galosphere:berserker" : [1,3],
-  "minecraft:creeper" : [3,6],
-  "minecraft:warden" : [1,1],
-  "minecraft:wither" : [1,1]
+
+  //entity : [siler(1)cut, golden(9)cut]  --- cut - chunk unlock token
+  "minecraft:elder_guardian" : [0,1],
+  "minecraft:ender_dragon" : [0,1],
+  "minecraft:warden" : [0,1],
+  "minecraft:wither" : [0,1],
+
+  "galosphere:berserker" : [0,1],
+
+  "block_factorys_bosses:underworld_knight": [0,1],
+  "block_factorys_bosses:yeti": [0,1],
+  "block_factorys_bosses:sandworm": [0,1],
+  "block_factorys_bosses:infernal_dragon": [0,1],
+
+  "mowziesmobs:ferrous_wroughtnaut": [0,1],
+  "mowziesmobs:umvuthi": [0,1],
+  "mowziesmobs:frostmaw": [0,1],
+  "mowziesmobs:naga": [2,0],
+  "mowziesmobs:sculptor": [0,1],
+
+  "legendary_monsters:shulker_mimic" : [1,0],
+  "legendary_monsters:skeletosaurus" : [2,0],
+  "legendary_monsters:withered_abomination": [2,0],
+  "legendary_monsters:lava_eater": [0,1],
+  "legendary_monsters:frostbitten_golem" : [2,0],
+  "legendary_monsters:cloud_golem" : [0, 2],
+  "legendary_monsters:ancient_guardian" : [0,1],
+  "legendary_monsters:overgrown_colossus" : [0,1],
+  "legendary_monsters:posessed_paladin" : [0,1],
+  "legendary_monsters:dune_sentinel" : [0, 1]
+
+
 }
