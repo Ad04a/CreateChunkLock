b467e7d4993c966a41161f9029007b8522158b92
Ad04a <andrey.todorov04@gmail.com>
2025-10-10 17:47:37 +0300

    cleaned l2 ecosystem + needs additional cleaning of unused files

--- kubejs/server_scripts/bosses.js
+++ kubejs/server_scripts/bosses.js
@@ -2,11 +2,11 @@
 
 ServerEvents.entityLootTables(event => {
   for(let [bossId, dropAmounts] of Object.entries(global.bossIds)) {
-    event.addEntity(bossId, table => {
+    event.modifyEntity(bossId, table => {
         table.addPool(pool => {
         pool.rolls = 1
         pool.addItem('kubejs:token')
-            .count([dropAmounts[0], dropAmounts[1]])
+            .count([dropAmounts[1], dropAmounts[1]])
         })
     })
   };
